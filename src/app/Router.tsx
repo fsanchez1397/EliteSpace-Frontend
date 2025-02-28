68bd5997da6bb905a1bb9ede139f1bfb482829b4
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2025-02-28 14:49:51 -0600

    Squashed commit of the following:
    commit 0f1217e9abd40693dcb1fd14d10f41195d021977
    Author: Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
    Date:   Fri Feb 28 12:51:51 2025 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/app/Router.tsx

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
+import HomePage from "./routes/HomePage";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<MockComponent />}
-        ></Route>
+        <Route path="/mock" element={<MockComponent />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/app/routes/HomePage.tsx
@@ -0,0 +1,9 @@
+// Tenant Dashboard once the tenant is logged in
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
