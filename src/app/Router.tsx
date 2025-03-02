4c57b8fc4a21f96fdaefbf71dbc6cc75370836f6
Felipe Sanchez <felipesanchez10@protonmail.com>
2025-03-02 14:04:25 -0500

    feat: Dashboard UI Layout

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
 import HomePage from "./routes/HomePage";
-
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/mock" element={<MockComponent />}></Route>
-        <Route path="/" element={<HomePage />}></Route>
+        {/* <Route path="/mock" element={<MockComponent />}></Route> */}
+        //{" "}
+        <Route
+          path="/"
+          element={<HomePage />}
+        ></Route>
       </Routes>
     </Router>
   );
