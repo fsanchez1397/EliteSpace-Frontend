36ef70160fb0ab26a34256117629336fd3437f93
FELIPE SANCHEZ <felipesanchez10@protonmail.com>
2025-03-04 03:33:40 -0500

    fix(RTK Query): Replaced RTK provider with Redux store integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
 import AppProvider from "./Provider";
 import AppRouter from "./Router";
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { userApi } from "../features/Services/userSlice";
+
 function App() {
   return (
     <AppProvider>
-      <ApiProvider api={userApi}>
-        <AppRouter />
-      </ApiProvider>
+      <AppRouter />
     </AppProvider>
   );
 }
