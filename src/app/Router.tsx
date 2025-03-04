23eb14cdc1969fcccdf9df4bfce3825b87bf18e3
FELIPE SANCHEZ <felipesanchez10@protonmail.com>
2025-03-04 04:12:07 -0500

    feat(Frontend Backend Integration): Added new api route that calls backend

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
+import TestApiCall from "../features/TestApiCall/TestApiCall";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<MockComponent />}
-        ></Route>
+        <Route path="/" element={<MockComponent />}></Route>
+        <Route path="/" element={<MockComponent />}></Route>
+        <Route path="/api-call" element={<TestApiCall />}></Route>
       </Routes>
     </Router>
   );
