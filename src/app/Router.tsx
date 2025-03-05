20bcf8f42a193188b145ff8f7460f66d28369e65
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-03-05 14:01:30 -0600

    forgot password UI

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
+import PasswordReset from "../features/PasswordReset/PasswordReset";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<MockComponent />}
-        ></Route>
+        <Route path="/" element={<MockComponent />}></Route>
+        <Route path="/password-reset" element={<PasswordReset />}></Route>
       </Routes>
     </Router>
   );
