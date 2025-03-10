9ef015c13c1b9df771a4e89c2c627db56df6ddf2
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-03-10 11:57:22 -0500

    clean up files to remove empty spaces

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,33 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { MockComponent } from "../features/MockFeature/MockComponent";
-
-
 import PasswordReset from "../features/PasswordReset/PasswordReset";
-
-
-
 import SignupPage from "../features/auth/SignupPage";
-
 import HomePage from "./routes/HomePage";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<MockComponent />}></Route>
         <Route path="/password-reset" element={<PasswordReset />}></Route>
-
         {/* <Route path="/mock" element={<MockComponent />}></Route> */}
-        <Route
-          path="/"
-          element={<HomePage />}
-        ></Route>
-
-
-
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="/signup" element={<SignupPage />} />
-
       </Routes>
     </Router>
   );
