414d944e964b3051dda68db4c5cc3aea9646e7f7
Maria Sanli <marisanli.93@gmail.com>
2025-03-09 23:27:40 -0400

    feat(guest): added mui icon and styling

--- src/features/GuestAccess/GuestAccess.tsx
+++ src/features/GuestAccess/GuestAccess.tsx
@@ -5,11 +5,14 @@ import {
   TextField,
   Button,
   Typography,
-  FormControl,
-  MenuItem,
   Select,
+  MenuItem,
+  FormControl,
+  Paper,
+  Box,
 } from "@mui/material";
 import { Link } from "react-router";
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
 
 const GuestAccess = () => {
   return (
@@ -19,96 +22,126 @@ const GuestAccess = () => {
         marginTop: "60px",
       }}
     >
-      <Stack
+      <Paper
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: { xs: "auto", sm: 500 },
-          height: "100%",
-          margin: "0 auto",
-          gap: "50px",
+          padding: "20px",
+          borderRadius: "10px",
         }}
       >
-        <Typography
-          sx={{
-            textAlign: "center",
-            fontWeight: 500,
-            fontSize: "1.5rem",
-            lineHeight: "1.2",
-          }}
-        >
-          Guest Access
-        </Typography>
         <Stack
           sx={{
-            width: "90%",
-            gap: "20px",
-          }}
-        >
-          <Stack
-            sx={{
-              width: "100%",
-              gap: "10px",
-            }}
-          >
-            <InputLabel>Guest Name</InputLabel>
-            <TextField
-              sx={{
-                width: "90%",
-                margin: "0 auto",
-              }}
-            />
-          </Stack>
-          <Stack
-            sx={{
-              width: "100%",
-              gap: "10px",
-            }}
-          >
-            <InputLabel>Time Limit</InputLabel>
-            {/* <TextField
-              sx={{
-                width: "30%",
-                margin: "0 auto",
-              }}
-            /> */}
-              <FormControl sx={{ width: "80%", margin: "0 auto" }}>
-              <Select defaultValue="1">
-                <MenuItem value="1">60 minutes</MenuItem>
-                <MenuItem value="2">2 hours</MenuItem>
-                <MenuItem value="4">4 hours</MenuItem>
-                <MenuItem value="8">8 hours</MenuItem>
-                <MenuItem value="24">24 hours (1 day)</MenuItem>
-                <MenuItem value="48">48 hours (2 days)</MenuItem>
-                <MenuItem value="168">1 week</MenuItem>
-              </Select>
-            </FormControl>
-          </Stack>
-        </Stack>
-        <Link
-          to="/guestacess/key"
-          style={{
-            textDecoration: "none",
-            width: "100%",
-            margin: "0 auto",
             display: "flex",
-            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            width: { xs: "auto", sm: 500 },
+            height: "100%",
+            margin: "0 auto",
+            gap: "30px",
           }}
         >
-          <Button
-            variant="contained"
-            sx={{
-              width: "50%",
-              height: "50px",
-              margin: "0 auto",
-              marginTop: "10px",
+
+          <Box 
+            sx={{ 
+              display: "flex", 
+              justifyContent: "center",
+              marginBottom: "-20px"
             }}
           >
-            Generate Key
-          </Button>
-        </Link>
-      </Stack>
+            <VpnKeyIcon 
+              sx={{ 
+                fontSize: 60, 
+                color: "primary.main",
+              }} 
+            />
+          </Box>
+          
+          <Typography
+            sx={{
+              textAlign: "center",
+              fontWeight: 500,
+              fontSize: "1.5rem",
+              lineHeight: "1.2",
+            }}
+          >
+            Guest Access
+          </Typography>
+          
+          <Typography 
+            variant="body2" 
+            color="text.secondary"
+            sx={{ 
+              textAlign: "center",
+              marginTop: "-20px",
+              marginBottom: "10px"
+            }}
+          >
+            Generate a temporary access key for you visitors
+          </Typography>
+          
+          <Stack
+            sx={{
+              width: "90%",
+              gap: "20px",
+            }}
+          >
+            <Stack
+              sx={{
+                width: "100%",
+                gap: "10px",
+              }}
+            >
+              <InputLabel>Guest Name</InputLabel>
+              <TextField
+                sx={{
+                  width: "90%",
+                  margin: "0 auto",
+                }}
+              />
+            </Stack>
+            <Stack
+              sx={{
+                width: "100%",
+                gap: "10px",
+              }}
+            >
+              <InputLabel>Time Limit</InputLabel>
+              <FormControl sx={{ width: "80%", margin: "0 auto" }}>
+                <Select defaultValue="1">
+                  <MenuItem value="1">60 minutes</MenuItem>
+                  <MenuItem value="2">2 hours</MenuItem>
+                  <MenuItem value="4">4 hours</MenuItem>
+                  <MenuItem value="8">8 hours</MenuItem>
+                  <MenuItem value="24">24 hours (1 day)</MenuItem>
+                  <MenuItem value="48">48 hours (2 days)</MenuItem>
+                  <MenuItem value="168">1 week</MenuItem>
+                </Select>
+              </FormControl>
+            </Stack>
+          </Stack>
+          <Link
+            to="/guestaccess/key"
+            style={{
+              textDecoration: "none",
+              width: "100%",
+              margin: "0 auto",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                width: "50%",
+                height: "50px",
+                margin: "0 auto",
+                marginTop: "10px",
+              }}
+            >
+              Generate Key
+            </Button>
+          </Link>
+        </Stack>
+      </Paper>
     </Container>
   );
 };
