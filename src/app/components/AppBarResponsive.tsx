44368ec323023e42c6bb73ed05357f938d4cbbde
Taryn King <49492414+tk1ng@users.noreply.github.com>
2025-03-15 12:53:59 -0500

    ES-93: refactor to fix build errors for deploying (#22)
    * chore: small refactors to clear build errors for deploying
    
    * chore(build-error): reinstall emotion, add clean script
    
    * chore(missing package): reinstall emotion/styled
    
    ---------
    
    Co-authored-by: Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>

--- src/app/components/AppBarResponsive.tsx
+++ src/app/components/AppBarResponsive.tsx
@@ -12,7 +12,6 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 
 const pages = ["Tenant Support", "Smart Package Locker", "Guest Access", "Guest Parking", "Digital Lease"];
-
 const settings = ["Profile", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
