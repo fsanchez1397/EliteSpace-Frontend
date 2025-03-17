4e76c63646b34bbe35f856882b48a24cf813c771
Patrick <118770089+Patrick-250@users.noreply.github.com>
2025-03-16 22:52:34 -0500

    correct build errors

--- src/features/PasswordReset/PasswordReset.tsx
+++ src/features/PasswordReset/PasswordReset.tsx
@@ -17,7 +17,7 @@ const PasswordReset: React.FC = () => {
     setSuccess(false);
 
     try {
-      const response = await fetch("http://localhost:3000/auth/forgot-password") {
+      const response = await fetch("http://localhost:3000/auth/forgot-password", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
