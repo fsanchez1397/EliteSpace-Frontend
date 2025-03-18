92ea3e361d14c36c4703758532436ee2a271fa10
FELIPE SANCHEZ <110352231+fsanchez1397@users.noreply.github.com>
2025-03-18 17:54:47 -0400

    ES-76 feat: AI escalation UI (#24)
    * feat(form): Added form validation, image selection and submit
    
    * fix(diverged) pulled remote to fix diverged issue
    
    * fix() removed unused funtion
    
    * wip: validated users session from frontend and working on users connection to supabase

--- src/features/Login/api/loginApi.ts
+++ src/features/Login/api/loginApi.ts
@@ -9,6 +9,8 @@ export const loginApi = createApi({
   reducerPath: 'loginApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:3000/',
+
+    credentials: 'include',
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
--- src/features/Services/userSlice.ts
+++ src/features/Services/userSlice.ts
@@ -2,17 +2,32 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 interface User {
   message: string;
 }
+interface Complaint {
+  selectedIssue: string;
+  extraDetails?: string;
+  img?: string;
+}
 export const userApi = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/',
+    credentials: 'include',
+  }),
 
   endpoints: (builder) => ({
-    // Define the shape of the data returned by this endpoint
+    // Remove
     getAllUsers: builder.query<User, void>({
       //Calls the endpoint at http://localhost:3000/api/ping
       query: () => `ping`,
     }),
+    sendComplaint: builder.mutation({
+      query: (complaint: Complaint) => ({
+        url: `/complaints/submit-complaint`,
+        method: 'POST',
+        body: complaint,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllUsersQuery } = userApi;
+export const { useGetAllUsersQuery, useSendComplaintMutation } = userApi;
