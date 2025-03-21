e1809065d3136e78c5a99497fb6039c41d5bba09
Micheline <MDoughlin@hotmail.com>
2025-03-20 21:39:18 -0400

    (style) centered text and added hover

--- src/features/Dashboard/components/DashboardCard.tsx
+++ src/features/Dashboard/components/DashboardCard.tsx
@@ -14,9 +14,21 @@ export default function OutlinedCard({ title, height, path }: CardProps) {
   return (
     <Box sx={{ minWidth: 275, flex: 1 }}>
       <Link to={path} style={{ textDecoration: 'none' }}>
-        <Card variant='outlined' title={title} sx={{ height: height }}>
+        <Card
+          variant='outlined'
+          title={title}
+          sx={{
+            height: height,
+            border: '1px solid #1a3b5d',
+            transition: 'border 0.3s ease-in-out',
+            '&:hover': { border: '1px solid #28a2a2' },
+          }}
+        >
           <CardContent>
-            <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+            <Typography
+              gutterBottom
+              sx={{ textAlign: 'center', color: 'text.secondary', fontSize: 14 }}
+            >
               {title}
             </Typography>
           </CardContent>
