e4f9ed8af685bfff78df4021a6f832a091c059e2
Micheline <MDoughlin@hotmail.com>
2025-03-22 00:47:41 -0400

    Background color added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,20 @@
 import AppProvider from './Provider';
 import AppRouter from './Router';
+import background from '../assets/Background.png';
 
 function App() {
   return (
-    <AppProvider>
-      <AppRouter></AppRouter>
-    </AppProvider>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        minHeight: '100vh',
+      }}
+    >
+      <AppProvider>
+        <AppRouter></AppRouter>
+      </AppProvider>
+    </div>
   );
 }
 
