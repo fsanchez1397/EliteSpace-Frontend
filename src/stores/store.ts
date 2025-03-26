cd7b6d4a556e071f9fb9b5e85252cc9be8488548
Eddie Maceda <eddiemaceda11@gmail.com>
2025-03-25 18:24:14 -0700

    fix(parking): fix merge/build conflicts

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -25,7 +25,10 @@ const store = configureStore({
     accessCodes: accessCodesReducer,
     tenant: tenantReducer,
     user: userReducer,
+<<<<<<< HEAD
     [guestParkingApi.reducerPath]: guestParkingApi.reducer,
+=======
+>>>>>>> f773bdc (fix(parking): fix merge/build conflicts)
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
